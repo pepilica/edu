50ed9b89ddd29b007a52b83e92a206cd32a0ac7f
pepilica <57628520+pepilica@users.noreply.github.com>
2019-11-11 17:57:08 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from random import choice
+import sys
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('circles.ui', self)
+        self.btn.clicked.connect(self.button)
+
+    def button(self):
+        self.update()
+
+    def paintEvent(self, QPaintEvent):
+        qp = QPainter()
+        qp.begin(self)
+        self.circles(qp)
+        qp.end()
+
+    def circles(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(choice(1, 4)):
+            size = choice(range(300))
+            qp.drawEllipse(choice(range(300)), choice(range(300)),
+                           size, size)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
